cddd7a14663f7f722c0a51ae10fa4b76f262bda2
Qiongsi Wu <qiongsiwu@gmail.com>
2020-01-14 15:39:46 -0500

    ObjFieldStore: adding a test case for struct arrays

---
+++ test/inputs/ObjFieldStore/struct_arr.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+struct foo{
+    struct foo *next; 
+}; 
+
+int main() {
+    struct foo f;
+    f.next = NULL;
+    struct foo **fptrs;
+    fptrs = malloc(3 * sizeof(struct foo *)); 
+    fptrs[2] = NULL; 
+    struct foo *fptrs_arr[3]; 
+    fptrs_arr[2] = NULL; 
+    printf("hello foo\n"); 
+}
